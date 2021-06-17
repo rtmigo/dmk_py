0e432b60d9a429427bf69d755c496b283d5126eb
rtmigo <art.galkin@gmail.com>
2021-06-17 20:37:48 +0300

    before replacing fakes

--- codn/c_namegroups/_fakes.py
+++ codn/c_namegroups/_fakes.py
@@ -1,14 +1,14 @@
-# SPDX-FileCopyrightText: (c) 2021 Artёm IG <github.com/rtmigo>
-# SPDX-License-Identifier: MIT
-
-from Crypto.Random import get_random_bytes
-
-from codn._common import CLUSTER_SIZE
-from codn.a_base import CodenameKey, Imprint
-
-
-def create_fake_bytes(fpk: CodenameKey) -> bytes:
-    result = Imprint(fpk).as_bytes + get_random_bytes(
-        CLUSTER_SIZE - Imprint.FULL_LEN)
-    assert len(result) == CLUSTER_SIZE
-    return result
+# # SPDX-FileCopyrightText: (c) 2021 Artёm IG <github.com/rtmigo>
+# # SPDX-License-Identifier: MIT
+#
+# from Crypto.Random import get_random_bytes
+#
+# from codn._common import CLUSTER_SIZE
+# from codn.a_base import CodenameKey, Imprint
+#
+#
+# def create_fake_bytes(fpk: CodenameKey) -> bytes:
+#     result = Imprint(fpk).as_bytes + get_random_bytes(
+#         CLUSTER_SIZE - Imprint.FULL_LEN)
+#     assert len(result) == CLUSTER_SIZE
+#     return result
