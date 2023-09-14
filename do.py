3f91b8d5d080ac525d2782ea7b9163c56cf57375
Artem IG <ortemeo@gmail.com>
2023-09-14 20:04:33 +0300

    - replacing CI with do.py - fixing windows unicode errors

--- do.py
+++ do.py
@@ -29,7 +29,13 @@ def _test():
 @app.command()
 def test_pkg():
     with Package() as pkg:
+        # pkg.run_shell_code('dmk --version')
+        pkg.run_shell_code('dmk --help')
+        # pkg.run_shell_code('dmk', expected_return_code=2)
         pkg.run_shell_code('dmk --version')
+        # todo problem with unicode: is it chkpkg or dmk problem?
+        pkg.run_python_code("from dmk import DmkFile, get_text, set_text, "
+                            "get_file, set_file")
     print("\nPackage is OK!")
 
 
--- test_unit.py
+++
@@ -1,4 +0,0 @@
-import neatest
-
-if __name__ == "__main__":
-    neatest.run()
